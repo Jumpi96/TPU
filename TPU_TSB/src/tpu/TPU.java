1211e090ac7d8c2fc021fdd6b1fef1d9cc7452f2
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2016-10-05 17:23:21 -0300

    CambioNombre

--- TPU_TSB/src/tpu/TPU.java
+++ TPU_TSB/src/tpu/TPU.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tpu_tsb;
+package tpu;
 
 /**
  *
  * @author juamp
  */
-public class TPU_TSB {
+public class TPU {
 
     /**
      * @param args the command line arguments
