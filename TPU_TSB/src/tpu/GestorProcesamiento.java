93f57e2880b5081f1f32865b639facbb59799bd4
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2016-10-07 21:13:25 -0300

    Modificado para que Hash pueda contar cantidad de archivos que aparece

--- TPU_TSB/src/tpu/GestorProcesamiento.java
+++ TPU_TSB/src/tpu/GestorProcesamiento.java
@@ -21,7 +21,7 @@ import java.util.logging.Logger;
  */
 public class GestorProcesamiento {
     private String origen;
-    private HashMap <String,Integer>  hash;
+    private HashMap <String,int[]>  hash;
 
     public GestorProcesamiento(String origen) {
         this.origen = origen;
@@ -39,7 +39,8 @@ public class GestorProcesamiento {
         StringSimbolizador st;
         String linea,cont;
         Charset inputCharset = Charset.forName("ISO-8859-1");
-
+        int[] a;
+        boolean primeraVezEnArchivo=true;
         try {
             br = new BufferedReader(new InputStreamReader(new FileInputStream(origen), inputCharset));
             linea=br.readLine();
@@ -49,10 +50,17 @@ public class GestorProcesamiento {
                     while(st.hasMoreTokens()){
                         cont=st.nextToken();
                         if(cont!=null)
-                            if(hash.containsKey(cont)) 
-                                hash.put(cont,hash.get(cont)+1);
+                            if(hash.containsKey(cont)) {
+                                a=hash.get(cont);
+                                a[0]=a[0]+1;
+                                if(primeraVezEnArchivo){
+                                    a[1]=a[1]+1;
+                                    primeraVezEnArchivo=false;
+                                }
+                                hash.put(cont,a);
+                            }
                             else
-                                hash.put(cont,1);
+                                hash.put(cont,new int[]{1,1});
                     }
                 }   
                 linea=br.readLine();
