ec506c8f20abbf5b96d9b92e2d02f516f29cf801
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2016-10-10 03:07:13 -0300

    Conexion con BD, BD creada vacia Arreglado algoritmo del Gestor

--- TPU_TSB/src/tpu/TPU.java
+++ TPU_TSB/src/tpu/TPU.java
@@ -6,6 +6,11 @@
 package tpu;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,9 +21,19 @@ public class TPU {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(TPU.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         GestorProcesamiento g = new GestorProcesamiento("16082-8.txt");
         g.procesar();
     }
     
+    private static void baseDatos() throws SQLException{
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\Facultad\\TSB\\TPU\\Repositorio\\TPU\\TPU_TSB\\vocabulario");
+        //seguir
+    }
 }
